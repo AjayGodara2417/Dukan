50d87ac2cc7f5979ec437a93fe8a5318da41f9c6
Ajay Godara <158254090+AjayGodara2417@users.noreply.github.com>
2025-04-11 17:57:43 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -28,11 +28,6 @@ const createRouter = (io) => {
     res.status(200).json({ message: "Product added successfully" });
   });
 
-    // Serve frontend for any unknown route
-    router.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../client/dist/index.html')); // Adjust path if needed
-    });
-
 
   return router;
 };
